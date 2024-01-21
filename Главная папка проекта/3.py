8ba3769e39af42ae5a27b660c23c728327497cc1
JleLLluy <151763535+JleLLluy@users.noreply.github.com>
2024-01-21 19:05:43 +0300

    Delete Главная папка проекта directory

--- Главная папка проекта/3.py
+++
@@ -1,5 +0,0 @@
-def run(self):
-    name, ok_pressed = QInputDialog.getText(self, "Введите имя",
-                                            "Как тебя зовут?")
-    if ok_pressed:
-        self.info_btn.setText(name)
--- Главная папка проекта/check.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-weap = input()
-con = sqlite3.connect("db_for_attack")
-cur = con.cursor()
-result = cur.execute("""SELECT * FROM for_attack WHERE weapon = ?""", (weap,)).fetchone()
-for elem in result:
-    print(elem)
-con.close()
